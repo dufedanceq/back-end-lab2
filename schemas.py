0225c7dd86c9e0837dd7a12d24b55c1f44069908
dufedanceq <nikiforovartem000@gmail.com>
2025-12-16 17:33:13 +0200

    updatde userschema and add userloginschema

--- schemas.py
+++ schemas.py
@@ -7,7 +7,12 @@ class CurrencySchema(Schema):
 class UserSchema(Schema):
     id = fields.Str(dump_only=True)
     name = fields.Str(required=True)
+    password = fields.Str(required=True, load_only=True) 
     default_currency_id = fields.Int(required=False)
+
+class UserLoginSchema(Schema):
+    name = fields.Str(required=True)
+    password = fields.Str(required=True)
 
 class CategorySchema(Schema):
     id = fields.Str(dump_only=True)
