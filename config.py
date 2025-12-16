aa1d68a3e188c08039ef3b9b11b9263a8a4be617
dufedanceq <nikiforovartem000@gmail.com>
2025-12-16 09:24:41 +0200

    apply migrations

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 import os
 
-class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv(
-        'DATABASE_URL', 
-        'postgresql://user:password@localhost:5432/expenses'
-    )
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    API_TITLE = "Expense Tracker API"
-    API_VERSION = "v1"
-    OPENAPI_VERSION = "3.0.3"
-    OPENAPI_URL_PREFIX = "/"
-    OPENAPI_SWAGGER_UI_PATH = "/swagger-ui"
-    OPENAPI_SWAGGER_UI_URL = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+SQLALCHEMY_DATABASE_URI = os.getenv(
+    'DATABASE_URL', 
+    'postgresql://user:password@localhost:5432/expenses'
+)
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+API_TITLE = "Expense Tracker API"
+API_VERSION = "v1"
+OPENAPI_VERSION = "3.0.3"
+OPENAPI_URL_PREFIX = "/"
+OPENAPI_SWAGGER_UI_PATH = "/swagger-ui"
+OPENAPI_SWAGGER_UI_URL = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
