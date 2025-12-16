f47b357e2fd6bf7c0cc3298c2f07bb2a53ac038f
dufedanceq <nikiforovartem000@gmail.com>
2025-12-16 17:29:06 +0200

    added JWT_SECRET_KEY

--- config.py
+++ config.py
@@ -12,3 +12,5 @@ OPENAPI_VERSION = "3.0.3"
 OPENAPI_URL_PREFIX = "/"
 OPENAPI_SWAGGER_UI_PATH = "/swagger-ui"
 OPENAPI_SWAGGER_UI_URL = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+
+JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "super-secret-key-change-me")
