b599e01d345edc06d9a0b878727222dabbbcff28
dufedanceq <nikiforovartem000@gmail.com>
2025-12-16 09:20:41 +0200

    added marshmallow schemas for validation

---
+++ schemas.py
@@ -0,0 +1,26 @@
+from marshmallow import Schema, fields
+
+class CurrencySchema(Schema):
+    id = fields.Int(dump_only=True)
+    name = fields.Str(required=True)
+
+class UserSchema(Schema):
+    id = fields.Str(dump_only=True)
+    name = fields.Str(required=True)
+    default_currency_id = fields.Int(required=False)
+
+class CategorySchema(Schema):
+    id = fields.Str(dump_only=True)
+    name = fields.Str(required=True)
+
+class RecordSchema(Schema):
+    id = fields.Str(dump_only=True)
+    user_id = fields.Str(required=True)
+    category_id = fields.Str(required=True)
+    currency_id = fields.Int(load_default=None)
+    amount = fields.Float(required=True)
+    timestamp = fields.DateTime(dump_only=True)
+
+class RecordQuerySchema(Schema):
+    user_id = fields.Str(required=False)
+    category_id = fields.Str(required=False)
